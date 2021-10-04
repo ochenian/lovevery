b795df8f7f2334ffd972ddadf88f1d754b5d6ce6
bkent <35678319+ochenian@users.noreply.github.com>
2021-10-03 18:14:35 -0700

    Add Breadcrumbs component

---
+++ src/components/Breadcrumbs/Styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { color } from '../../styles/constants';
+
+export const Container = styled.div`
+  color: ${color.textMedium};
+  font-size: 0.75rem;
+`;
+
+export const Divider = styled.span`
+  margin: 0 10px;
+`;
---
+++ src/components/Breadcrumbs/index.jsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import { Container, Divider } from './Styles';
+
+const propTypes = {
+  items: PropTypes.array.isRequired,
+};
+
+const Breadcrumbs = ({ items }) => (
+  <Container>
+    {items.map((item, index) => {
+      const lastIndex = items.length - 1;
+      return (
+        <Fragment key={item}>
+          {index !== 0 && <Divider>{'>'}</Divider>}
+          {index !== lastIndex ? (
+            <span style={{ fontWeight: 'bold' }}>{item}</span>
+          ) : (
+            <>{item}</>
+          )}
+        </Fragment>
+      );
+    })}
+  </Container>
+);
+
+Breadcrumbs.propTypes = propTypes;
+
+export default Breadcrumbs;
